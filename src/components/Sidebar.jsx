4049284803659ca57124c185d9be6742e66a6c45
Pasindu107 <154233093+Pasindu107@users.noreply.github.com>
2024-05-20 06:43:21 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,14 +10,14 @@ const Sidebar = () => {
   return (
 
     
-    <div className='relative flex-col  min-w-[10px] hidden sm:block md:min-w-[250px] lg:min-w-[300px] xl:min-w-[300px] border-r min-h-full p-4 bg-white rounded-[10px]'>
-        <div className='hidden sm:hidden md:block lg:block xl:block text-[25px] min-h-[100px] p-4'>
+    <div className='relative flex-col hidden sm:hidden md:block lg:block xl:block lg:min-w-[300px] xl:min-w-[300px] border-r min-h-full p-4 bg-white rounded-[10px]'>
+        <div className='text-[25px] min-h-[100px] p-4'>
           ADMIN PANEL
         </div>
         <Link href={"/"}>
-          <div className='gap-2 px-2 py-3 bg-gray-50 hover:bg-purple-200 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+          <div className='gap-2 px-2 py-3 bg-gray-50 hover:bg-purple-200 rounded-lg my-3 p-2 grid  sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
             <HomeIcon />
-            <div className='hidden sm:hidden md:block lg:block xl:block'>
+            <div className=''>
               Home
             </div>
                
@@ -26,7 +26,7 @@ const Sidebar = () => {
         <Link href={"/sales"}>
           <div className='gap-2 px-2 py-3 bg-gray-50 hover:bg-purple-200 rounded-lg my-3 p-2 grid md:grid-cols-4 md:min-w-[20px] sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
             <BarChart /> 
-            <div className='hidden sm:hidden md:block lg:block xl:block'>
+            <div className=''>
               Sales
             </div>  
           </div> 
@@ -34,7 +34,7 @@ const Sidebar = () => {
         <Link href={"/items"}>
           <div className='gap-2 px-2 py-3 bg-gray-50 hover:bg-purple-200 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
             <ListChecks /> 
-            <div className='hidden sm:hidden md:block lg:block xl:block'>
+            <div className=''>
               Items
             </div>  
           </div> 
